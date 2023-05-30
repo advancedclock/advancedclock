fc1c3d34aa038c1a8261a161338b86ef97eef2d8
jjgroenendijk <3110270+jjgroenendijk@users.noreply.github.com>
2023-05-30 20:24:32 +0200

    updated functions

--- src/clock.c
+++ src/clock.c
@@ -3,15 +3,20 @@
 
 #include <MKL25Z4.h>
 #include <stdbool.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "clock.h"
 #include "led.h"
-
-#include "delay.h"		// only used for debugging. Delete from IRQhandler later
-
+#include "datetime.h"
 
 #define RECENTUNIXTIME 1685449404		// Unix tijd voor 2023-05-30 12:23:24 (UTC)
 
-
+volatile uint32_t unixTimeSeconds = 0;
+volatile datetime_t unixTime = {0, 0, 0, 0, 0, 0};
+volatile bool timeSyncFlag = false;
 
 void PITInit(uint32_t PITfrequency)
 {
@@ -47,7 +52,7 @@ void PIT_IRQHandler(void) {
     static uint32_t count = 0;
     count++;
 
-    // Toggle the LED every 1 second
+    // Toggle the LED every .2 second
     if (count == 1)
 		{
 				// Blue light
@@ -58,7 +63,67 @@ void PIT_IRQHandler(void) {
 		{
 				// Green light
 				setLEDStatus(false, true, false);
-				count = 0;
 		}
 		
+		if (count == 9)
+		{
+				unixTimeSeconds++;
+				count = 0;
+		}
+}
+
+// Check if time is in sync by comparing current time with a known recent timestamp
+void timeSyncCheck(uint32_t * unixTimeSeconds)
+{
+		if (*unixTimeSeconds < RECENTUNIXTIME)
+		{
+				timeSyncFlag = false;
+		}
+		
+		if (*unixTimeSeconds > RECENTUNIXTIME)
+		{
+				timeSyncFlag = true;
+		}
+}
+
+char* getTimeAsString(uint32_t unixTimeSeconds)
+{
+		// Preferred format: HH:MM:SS
+		// Init variables
+		char timeString[9];
+	
+		// Send UnixTimeSeconds to datetime.c
+		RTC_HAL_ConvertSecsToDatetime(&unixTimeSeconds, &unixTime);
+	
+		// Put time in local variables
+		uint32_t hour = unixTime.hour;
+		uint32_t minute = unixTime.minute;
+		uint32_t second = unixTime.second;
+		
+		// Put variables in string using sprintf
+		sprintf(timeString, "%02d:%02d:%02d", hour, minute, second);
+		
+		// return a pointer to the string
+		return timeString;
+}
+
+char* getDateAsString(uint32_t unixTimeSeconds)
+{
+		// Preferred format: DD/MM/YYYY
+		// Init variables
+		char dateString[9];
+	
+		// Send UnixTimeSeconds to datetime.c
+		RTC_HAL_ConvertSecsToDatetime(&unixTimeSeconds, &unixTime);
+	
+		// Put time in local variables
+		uint32_t day = unixTime.day;
+		uint32_t month = unixTime.month;
+		uint32_t year = unixTime.year;
+		
+		// Put variables in string using sprintf
+		sprintf(dateString, "%02d/%02d/%02d", day, month, year);
+	
+		// convert datetime_t struct to string
+		return dateString;
 }
